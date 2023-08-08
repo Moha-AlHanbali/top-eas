560b1e7596eb237adc2ec95cc489853c311292dd
Moha-AlHanbali <moha.alhanbali@gmail.com>
2023-08-09 01:24:54 +0300

    feat:  adjustable grid

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 "use strict";
 
-const gridContainer = document.getElementsByClassName("grid-container")[0];
+const boardContainer = document.getElementsByClassName("board-container")[0];
 
-const gridArray = [];
+let gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container")
+boardContainer.appendChild(gridContainer)
 
-for (let i = 0; i < 16 * 16; i++){
+
+for (let i = 0; i < 16 * 16; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add("grid-square", `square-${i}`);
-    gridArray.push(gridSquare);
     gridContainer.appendChild(gridSquare);
 }
+
+const gridResizeForm = document.getElementsByClassName("grid-size-form")[0];
+gridResizeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const gridX = e.target.gridX.value;
+    const gridY = e.target.gridY.value;
+
+    if (!gridX || !gridY) {
+        alert("Please enter both grid dimensions");
+        return;
+    }
+
+    gridContainer.remove()
+    gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    boardContainer.appendChild(gridContainer);
+
+    for (let i = 0; i < gridX * gridY; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add("grid-square", `square-${i}`);
+        gridSquare.style.flex = `1 1 ${100 / gridX}%`;
+        gridContainer.appendChild(gridSquare);
+    }
+})
